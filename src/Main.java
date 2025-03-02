40e5a826bb9fb207985c9c906edb2a220c5c97d9
michalbukalski <michalbukalski@gmail.com>
2025-03-02 21:51:40 +0100

    zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        int countOfMatches = 0;
+        int sumOfMatches = 0;
+
+        for (int loopNumber = 1; loopNumber <= 1000; loopNumber++){
+            if ((loopNumber % 3 == 0) && (loopNumber % 5 == 0)) {
+                countOfMatches++;
+                sumOfMatches += loopNumber;
+
+                System.out.println("Found a match = " + loopNumber);
+
+            }
+
+            if(countOfMatches == 5){
+                System.out.println("Sum of founded matches = " + sumOfMatches);
+                break;
+            }
+        }
+    }
 }
